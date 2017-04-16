0f7467877b13677f7308378feef082a97c60a3ac
Tony Brix <Tony@Brix.ninja>
2017-04-15 23:51:29 -0500

    init

---
+++ spec/test-engines-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TestEnginesPackageView from '../lib/test-engines-package-view';
+
+describe('TestEnginesPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
